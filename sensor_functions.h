16c31424fae7b6ef7b5746236922c9af7b3b452e
eSGarden: School Gardens for Future Citizens <68754412+esgardenArduino@users.noreply.github.com>
2021-02-02 14:04:47 +0100

    Create sensor_functions.h
    Sensors descriptions

---
+++ sensor_functions.h
@@ -0,0 +1,62 @@
+/*************************************************************************************/
+/** Implement your read sensor functions here ****************************************/
+/*************************************************************************************/
+#include "dht.h"
+dht DHT; //Include your libraries 
+
+
+int readCO2(int pin); // air quality value
+int readDHT_t (int pin); // ambient temperature
+int readDHT_h (int pin); // ambient humidity
+int readBgt (int pin); // brightness
+int readW_f (int pin); // wind force
+int readW_d (int pin); // wind direction
+int readWLevel (int pin); // water level in a pluviometer
+int readSoilHum (int pin); //soil moisture
+
+
+/************** BASIC READING FUNCTION  Example **************************************/
+int ReadSensor(int pin) {
+  int output_value = analogRead(pin); //analog or digital; remove and make your own function here
+  return output_value; //change the return by int or float
+}
+/************** AD-HOC READING FUNCTIONS **********************************************/
+
+
+/****** Soil moisture sensor: humidity; 1 value *********************************************/
+int readSoilHum (int pin) {
+  
+}
+/****** CO2 sensor: air quality; 1 value *********************************************/
+int readCO2(int pin) {
+  
+}
+
+/****** DHT11 sensor: temperature; 1 value *******************************************/
+int readDHT_t (int pin) {
+  
+}
+
+/****** DHT11 sensor: humidity; 1 value *********************************************/
+int readDHT_h (int pin) {
+ 
+}
+
+/****** Brightness detection sensor: lux; 1 value *********************************/
+int readBgt (int pin) {
+    
+
+/****** Wind sensor: force; 1 value *************************************************/
+int readW_f (int pin) {
+  
+}
+
+/****** Wind sensor: direction; 1 value *********************************************/
+int readW_d (int pin) {
+  
+}
+
+/****** Pluviometric sensor: level; 1 value *********************************************/
+int readWLevel (int pin) {
+    
+}
